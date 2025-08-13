5b2f206eb0c00e9b8a44779086bbb31340c8aabf
Christopher Hicks <chicks.net@gmail.com>
2025-08-13 08:14:18 -0700

    🔭 [diagrams] add monitoring and edge labels to diagram (#8)

--- architecture/diagrams/static-web.py
+++ architecture/diagrams/static-web.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env -S uv run --script
 
-from diagrams import Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.azure.general import Usericon
 from diagrams.digitalocean.network import Firewall
 from diagrams.digitalocean.storage import Space
+from diagrams.onprem.monitoring import Prometheus
 
 with Diagram("FINI Static Web Serving", show=False):
     user = Usericon("web user")
     cdn = Firewall("CDN")
     storage = Space("bucket")
+    monitoring = Prometheus("Prometheus/Grafana")
 
-    user >> cdn >> storage
+    # user traffic
+    user >> Edge(label="web public") >> cdn >> Edge(label="web internal") >> storage
 
-print("Done generating diagrams")
+    # monitoring
+    cdn >> Edge(label="metrics", style="dashed") >> monitoring
+    storage >> Edge(label="metrics", style="dashed") >> monitoring
+
+
+
+print("Done generating diagrams, no preview is coming.")
