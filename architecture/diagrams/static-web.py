2d8cf5bb4913a344e374afe9e4e0c3c58c89e6d9
Christopher Hicks <chicks.net@gmail.com>
2025-08-21 06:57:37 -0700

    🔎 [diagrams] clearer lettering and better metadata

--- architecture/diagrams/static-web.py
+++ architecture/diagrams/static-web.py
@@ -6,7 +6,27 @@ from diagrams.digitalocean.network import Firewall
 from diagrams.digitalocean.storage import Space
 from diagrams.onprem.monitoring import Prometheus
 
-with Diagram("FINI Static Web Serving", show=False):
+_graph_attr = {
+    "fontsize": "45",
+    "fontname": "Inter, Arial",
+    "labelloc": "t"
+}
+
+_node_attr={
+    "fontname": "Inter, Arial",
+    "fontsize": "20"
+}
+
+_edge_attr={
+    "fontname": "Inter, Arial",
+    "fontsize": "18",
+}
+
+with Diagram("FINI Static Web Serving",
+             graph_attr=_graph_attr, 
+             node_attr=_node_attr,
+             edge_attr=_edge_attr,
+             show=False):
     user = Usericon("web user")
     cdn = Firewall("CDN")
     storage = Space("bucket")
@@ -18,7 +38,5 @@ with Diagram("FINI Static Web Serving", show=False):
     # monitoring
     cdn >> Edge(label="metrics", style="dashed") >> monitoring
     storage >> Edge(label="metrics", style="dashed") >> monitoring
-
-
 
 print("Done generating diagrams, no preview is coming.")
