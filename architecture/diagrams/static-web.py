7f8464fc13c3c42a752302d2b044e060858a6e4c
Christopher Hicks <chicks.net@gmail.com>
2025-08-22 07:44:08 -0700

    🥋 [diagrams] uv format is experimental ruff

--- architecture/diagrams/static-web.py
+++ architecture/diagrams/static-web.py
@@ -9,24 +9,26 @@ from diagrams.onprem.monitoring import Prometheus
 _graph_attr = {
     "fontsize": "45",
     "fontname": "Inter, Arial",
-    "labelloc": "t"
+    "labelloc": "t",
 }
 
-_node_attr={
+_node_attr = {
     "fontname": "Inter, Arial",
-    "fontsize": "20"
+    "fontsize": "20",
 }
 
-_edge_attr={
+_edge_attr = {
     "fontname": "Inter, Arial",
     "fontsize": "18",
 }
 
-with Diagram("FINI Static Web Serving",
-             graph_attr=_graph_attr, 
-             node_attr=_node_attr,
-             edge_attr=_edge_attr,
-             show=False):
+with Diagram(
+    "FINI Static Web Serving",
+    graph_attr=_graph_attr,
+    node_attr=_node_attr,
+    edge_attr=_edge_attr,
+    show=False,
+):
     user = Usericon("web user")
     cdn = Firewall("CDN")
     storage = Space("bucket")
