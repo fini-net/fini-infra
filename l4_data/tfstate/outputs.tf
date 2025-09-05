390e5e09f5ae0239ca7e6a24061c52c3bbfbb9b1
Christopher Hicks <chicks.net@gmail.com>
2025-09-04 20:53:33 -0700

    💀 [terraform] skeleton for tfstate bucket (#18)
    * 💀 [terraform] skeleton for tfstate bucket
    
    * ran "tofu fmt ."

---
+++ l4_data/tfstate/outputs.tf
@@ -0,0 +1,9 @@
+output "environment" {
+  description = "Environment name"
+  value       = var.environment
+}
+
+output "region" {
+  description = "DigitalOcean region"
+  value       = var.region
+}
