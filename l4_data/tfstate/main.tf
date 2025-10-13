c2af825428ba03cdb8e6813152b6c2d725af23a3
Christopher Hicks <chicks.net@gmail.com>
2025-10-13 13:50:26 -0700

    💤 [tfstate] move provider-specific stuff in providers.tf (#28)

--- l4_data/tfstate/main.tf
+++ l4_data/tfstate/main.tf
@@ -1,32 +1,4 @@
-# comment
-
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-    onepassword = {
-      source  = "1Password/onepassword"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "onepassword" {
-  op_cli_path = var.onepassword_path
-}
-
-data "onepassword_item" "digocean_fini" {
-  vault = "Private"
-  title = "digocean-fini"
-}
-
-provider "digitalocean" {
-  token = data.onepassword_item.digocean_fini.credential
-}
+# main.tf
 
 resource "digitalocean_spaces_bucket" "terraform_state_bucket" {
   name   = "fini-terraform-state"
