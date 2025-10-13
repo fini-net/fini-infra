07caf73498f1ccf9173d580963394e819ad33f33
Christopher Hicks <chicks.net@gmail.com>
2025-10-13 11:51:13 -0700

    eliminate hard-coded username

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -1,4 +1,4 @@
-export OP_ACCOUNT=$(op account ls | grep chicks | awk '{print $3}')
+export OP_ACCOUNT=$(op account ls | awk 'NR==2 {print $3}')
 
 #export AWS_ACCESS_KEY_ID=$(op read op://Private/allbuckets-fini-2025/access_key)
 #export AWS_SECRET_ACCESS_KEY=$(op read op://Private/allbuckets-fini-2025/secret_key)
