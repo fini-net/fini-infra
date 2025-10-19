53047a7f4b7ba4ce128d3b890eb73b02842428a5
Christopher Hicks <chicks.net@gmail.com>
2025-10-19 15:19:12 -0700

    pick www.bettywalls.com and cleanups

--- l6_ingress/cdn-fini-domain-trust/main.tf
+++ l6_ingress/cdn-fini-domain-trust/main.tf
@@ -1,8 +1,12 @@
 # main.tf
 
+locals {
+  web_host_name = "www.bettywalls.com"
+}
+
 # Create a new Spaces Bucket
-resource "digitalocean_spaces_bucket" "origin_bucket" {
-  name   = "fini-origin-fini-doamain-trust"
+resource "digitalocean_spaces_bucket" "trust_origin_bucket" {
+  name   = "fini-origin-fini-domain-trust"
   region = var.region
   acl    = "public-read"
   versioning {
@@ -17,25 +21,25 @@ resource "digitalocean_spaces_bucket" "origin_bucket" {
   }
 }
 
+#TODO: enable certificate
 # Create a DigitalOcean managed Let's Encrypt Certificate
-#resource "digitalocean_certificate" "cert" {
+#resource "digitalocean_certificate" "trust_cert" {
 #  name    = "cdn-cert-fini-domain-trust"
 #  type    = "lets_encrypt"
-#  domains = ["static.example.com"]
+#  domains = [local.web_host_name]
 #}
 
 # Add a CDN endpoint with a custom sub-domain to the Spaces Bucket
-resource "digitalocean_cdn" "trust-cdn" {
-  origin           = digitalocean_spaces_bucket.origin_bucket.bucket_domain_name
-  custom_domain    = "static.example.com"
+resource "digitalocean_cdn" "trust_cdn" {
+  origin           = digitalocean_spaces_bucket.trust_origin_bucket.bucket_domain_name
+  custom_domain    = local.web_host_name
   #certificate_name = digitalocean_certificate.cert.name
 }
 
-#TODO: clean up
-resource "digitalocean_spaces_bucket_logging" "example" {
+resource "digitalocean_spaces_bucket_logging" "trust_logging" {
   region = var.region
-  bucket = digitalocean_spaces_bucket.origin_bucket.id
+  bucket = digitalocean_spaces_bucket.trust_origin_bucket.id
 
   target_bucket = data.terraform_remote_state.logs_cdn.outputs.logs_cdn_bucket_id
-  target_prefix = "access-logs/"
+  target_prefix = "access-logs/${local.web_host_name}/"
 }
--- l6_ingress/cdn-fini-domain-trust/outputs.tf
+++ l6_ingress/cdn-fini-domain-trust/outputs.tf
@@ -10,10 +10,15 @@ output "region" {
 
 output "origin_bucket_name" {
   description = "Name of the origin bucket"
-  value       = digitalocean_spaces_bucket.origin_bucket.name
+  value       = digitalocean_spaces_bucket.trust_origin_bucket.name
 }
 
 output "origin_bucket_urn" {
   description = "URN of the origin bucket"
-  value       = digitalocean_spaces_bucket.origin_bucket.urn
+  value       = digitalocean_spaces_bucket.trust_origin_bucket.urn
+}
+
+output "cdn_endpoint" {
+  description = "CDN endpoint"
+  value       = digitalocean_cdn.trust_cdn.endpoint
 }
