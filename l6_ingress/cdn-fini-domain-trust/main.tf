6f525980e0f29b681af6bdde6ac6a5bb5d1b9b24
Christopher Hicks <chicks.net@gmail.com>
2025-10-23 21:19:38 -0700

    go for the cert the first time

--- l6_ingress/cdn-fini-domain-trust/main.tf
+++ l6_ingress/cdn-fini-domain-trust/main.tf
@@ -24,17 +24,19 @@ resource "digitalocean_spaces_bucket" "trust_origin_bucket" {
 
 #TODO: enable certificate
 # Create a DigitalOcean managed Let's Encrypt Certificate
-#resource "digitalocean_certificate" "trust_cert" {
-#  name    = "cdn-cert-fini-domain-trust"
-#  type    = "lets_encrypt"
-#  domains = [local.web_host_name]
-#}
+resource "digitalocean_certificate" "trust_cert" {
+  name    = "cdn-cert-fini-domain-trust"
+  type    = "lets_encrypt"
+  domains = [local.web_host_name]
+}
 
 # Add a CDN endpoint with a custom sub-domain to the Spaces Bucket
 resource "digitalocean_cdn" "trust_cdn" {
-  origin        = digitalocean_spaces_bucket.trust_origin_bucket.bucket_domain_name
+  origin = digitalocean_spaces_bucket.trust_origin_bucket.bucket_domain_name
+
+  # custom_domain doesn't work without the certificate_name
   custom_domain = local.web_host_name
-  #certificate_name = digitalocean_certificate.cert.name
+  certificate_name = digitalocean_certificate.trust_cert.name
 }
 
 resource "digitalocean_spaces_bucket_logging" "trust_logging" {
