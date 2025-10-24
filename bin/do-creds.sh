8c7b04b81d8b85e7c23360279656bee039b511af
Christopher Hicks <chicks.net@gmail.com>
2025-10-24 06:06:44 -0700

    🎪 [just] automate tofu better and put plans in github comments (#42)
    * 🎪 [just] automate tofu better and put plans in github comments
    
    * clean up plan file automatically

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -1,5 +1,7 @@
 set -euo pipefail
 
+echo "Reading creds from 1password...."
+
 export OP_ACCOUNT=$(op account ls | awk 'NR==2 {print $3}')
 
 export AWS_ACCESS_KEY_ID=$(op read op://Private/allbuckets-fini-2025/access_key)
@@ -7,3 +9,5 @@ export AWS_SECRET_ACCESS_KEY=$(op read op://Private/allbuckets-fini-2025/secret_
 
 export SPACES_ACCESS_KEY_ID=$(op read op://Private/allbuckets-fini-2025/access_key)
 export SPACES_SECRET_ACCESS_KEY=$(op read op://Private/allbuckets-fini-2025/secret_key)
+
+echo "= creds DONE in ${SECONDS} seconds."
