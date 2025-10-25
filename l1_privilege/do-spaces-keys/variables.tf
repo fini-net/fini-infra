daf37553cf9731939f3cd295e05fafe2b60dbea4
Christopher Hicks <chicks.net@gmail.com>
2025-10-25 07:48:33 -0700

    🔑 [do-spaces-key] create Spaces keypair with opentofu (#44)
    * 🔑 [do-spaces-key] create Spaces keypair with opentofu
    
    * cleanups
    
    * explain how to get the generated keys

---
+++ l1_privilege/do-spaces-keys/variables.tf
@@ -0,0 +1,19 @@
+# input variables
+
+variable "region" {
+  description = "DigitalOcean region"
+  type        = string
+  default     = "nyc3" # only nyc region for DO Spaces
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "prod"
+}
+
+variable "onepassword_path" {
+  description = "Path to the 1password op command."
+  type        = string
+  default     = "op"
+}
