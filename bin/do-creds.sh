6351a8e0104d34f2899676e6ccdc7d624bd2cb17
Christopher Hicks <chicks.net@gmail.com>
2025-11-30 21:56:38 -0800

    🦡 [just] refactor to lighten each recipe by one line and allow tf-init options

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -11,3 +11,8 @@ export SPACES_ACCESS_KEY_ID=$(op read op://Private/allbuckets-fini-2025/access_k
 export SPACES_SECRET_ACCESS_KEY=$(op read op://Private/allbuckets-fini-2025/secret_key)
 
 echo "= creds DONE in ${SECONDS} seconds."
+
+if [[ -n "$1" ]]; then
+	cd "$1" || exit 1
+	#echo "cd success"
+fi
