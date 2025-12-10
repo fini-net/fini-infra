7c5a0bdd276d915271c3938c6b39392e8fdaed9f
Christopher Hicks <chicks.net@gmail.com>
2025-12-09 21:31:55 -0800

    use variable for hostname

--- l5_compute/mon-nagios/main.tf
+++ l5_compute/mon-nagios/main.tf
@@ -3,7 +3,7 @@
 # DigitalOcean Droplet for Nagios monitoring server
 # This droplet is imported from existing infrastructure
 resource "digitalocean_droplet" "mon_nagios" {
-  name   = "mon00.fini.net"
+  name   = var.hostname
   region = "nyc2"
   size   = "2gb"      # Legacy size slug - droplet created before standardized naming
   image  = "14782842" # CentOS 7.1 x64 - original image ID
