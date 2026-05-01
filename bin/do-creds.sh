2174949ee7766febe876d5bf6b8e3cde47f5d914
Christopher Hicks <chicks.net@gmail.com>
2026-05-01 15:30:56 -0700

    [tf] debug bin/do-creds.sh to handle being logged into multiple 1Password accounts
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -1,8 +1,10 @@
 set -euo pipefail
 
-echo "Reading creds from 1password...."
+if [[ -z "${OP_ACCOUNT:-}" ]]; then
+	export OP_ACCOUNT=$(op account ls --format=json 2>/dev/null | jq -r '.[0].account_uuid')
+fi
 
-export OP_ACCOUNT=$(op account ls | awk 'NR==2 {print $3}')
+echo "Reading creds from 1password...."
 
 export AWS_ACCESS_KEY_ID=$(op read op://Private/allbuckets-fini-2025/access_key)
 export AWS_SECRET_ACCESS_KEY=$(op read op://Private/allbuckets-fini-2025/secret_key)
