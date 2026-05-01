00030ab07c3e6018fc4a9fe7e532cfd86db2fc57
Christopher Hicks <chicks.net@gmail.com>
2026-05-01 16:20:41 -0700

    add newlines at EOF and a description
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>

--- l2_network/vpc-ric1/main.tf
+++ l2_network/vpc-ric1/main.tf
@@ -1,7 +1,8 @@
 # main.tf
 
 resource "digitalocean_vpc" "ric1" {
-  name     = "fini-ric1"
-  region   = var.region
-  ip_range = var.ip_range
+  name        = "fini-ric1"
+  description = "VPC for the DOKS cluster in the ric1 region"
+  region      = var.region
+  ip_range    = var.ip_range
 }
