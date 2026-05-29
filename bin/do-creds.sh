a2af3d4d72bdbf56201aa568e8449ab736150475
Christopher Hicks <chicks.net@gmail.com>
2026-05-28 21:01:24 -0400

    better comment about the magic string
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 command -v jq &>/dev/null || { echo "ERROR: jq is required but not installed." >&2; exit 1; }
 
 # Picks account whose user_uuid starts with "GBQ"; set OP_ACCOUNT in environment to override
+# This allows for additional accounts to be logged into 1Password and pick our own
+# 1Password acccount out of the list.
 if [[ -z "${OP_ACCOUNT:-}" ]]; then
 	OP_ACCOUNT=$(op account ls --format=json | jq -r '[.[] | select(.user_uuid | startswith("GBQ")).account_uuid][0] // empty')
 	if [[ -z "$OP_ACCOUNT" ]]; then
