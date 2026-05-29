92d84769c3d5669c935c169f94aadd0571501194
Christopher Hicks <chicks.net@gmail.com>
2026-05-28 21:05:41 -0400

    🗃️ [tf] improved bin/do-creds.sh to handle json order changing from 1Password (#110)
    * 🗃️ [tf] improved bin/do-creds.sh to handle json order changing from 1Password
    
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>
    
    * better comment about the magic string
    
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>
    
    * fix spelling in comment
    
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>
    
    ---------
    
    Signed-off-by: Christopher Hicks <chicks.net@gmail.com>

--- bin/do-creds.sh
+++ bin/do-creds.sh
@@ -2,14 +2,17 @@
 set -euo pipefail
 command -v jq &>/dev/null || { echo "ERROR: jq is required but not installed." >&2; exit 1; }
 
-# Uses first listed account; set OP_ACCOUNT in environment to override
+# Picks account whose user_uuid starts with "GBQ"; set OP_ACCOUNT in environment to override
+# This allows for additional accounts to be logged into 1Password and pick our own
+# 1Password account out of the list.
 if [[ -z "${OP_ACCOUNT:-}" ]]; then
-	OP_ACCOUNT=$(op account ls --format=json | jq -r '.[0].account_uuid // empty')
+	OP_ACCOUNT=$(op account ls --format=json | jq -r '[.[] | select(.user_uuid | startswith("GBQ")).account_uuid][0] // empty')
 	if [[ -z "$OP_ACCOUNT" ]]; then
 		echo "ERROR: No 1Password account detected. Run 'op signin' first." >&2
 		exit 1
 	fi
 	export OP_ACCOUNT
+	#echo "assigned OP_ACCOUNT=$OP_ACCOUNT"
 fi
 
 echo "Reading creds from 1password...."
